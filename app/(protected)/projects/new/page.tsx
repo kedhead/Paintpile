6499ca37dcb813e5b76a1c30bb295871a20db920
Draktus <ked4@cox.net>
2026-01-25 21:28:49 -0800

    fix(mobile): add bottom padding to create project page for better scrollability

--- app/(protected)/projects/new/page.tsx
+++ app/(protected)/projects/new/page.tsx
@@ -84,7 +84,7 @@ export default function NewProjectPage() {
   }
 
   return (
-    <div className="max-w-2xl mx-auto">
+    <div className="max-w-2xl mx-auto p-4 md:p-6 pb-32 md:pb-10">
       <Card>
         <CardHeader>
           <CardTitle>Create New Project</CardTitle>
